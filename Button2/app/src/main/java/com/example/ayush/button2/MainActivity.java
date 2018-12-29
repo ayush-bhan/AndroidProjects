87021be2c89b58b18ba856728847314c03bfc46e
Ayush Bhan <32763933+ayush-bhan@users.noreply.github.com>
2018-12-29 22:49:53 +0530

    init

---
+++ Button2/app/src/main/java/com/example/ayush/button2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ayush.button2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button) findViewById(R.id.idBtn);
+
+    }
+    public void btnClick(View v){
+        Toast.makeText(this,"Button Was Clicked",Toast.LENGTH_SHORT).show();
+    }
+}
